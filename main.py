b5fedd5728f80efccca22e54848ab2b2df16c384
obayram34 <oralbayram28@gmail.com>
2023-09-22 02:36:52 +0300

    exception, logger & utils  added

---
+++ main.py
@@ -0,0 +1,5 @@
+from mlProject import logger # normally  we should use src.mlProject
+                               # but  we have a ""__init__.py"""  in src folder
+
+
+logger.info(' This is  a trial log !!')
